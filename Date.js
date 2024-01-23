365b5e4474fca89d08661c0ca60f3216a99c051b
alralra <146728964+alralra@users.noreply.github.com>
2024-01-23 10:40:37 +0900

    Add files via upload

---
+++ Date.js
@@ -0,0 +1,15 @@
+// var now = new Date(2024, 0, 22);
+
+// // Date객체에서 월은 0부터 시작
+
+// console.log(now);
+
+var now = new Date();
+var date = now.getDate();
+var hour = now.getHours();
+console.log("Date()객체" + "<br>");
+console.log(now + "<br><br>");
+console.log("getDate()객체" + "<br>");
+console.log(date + "<br><br>");
+console.log("getHours()객체" + "<br>");
+console.log(hour + "<br><br>");
